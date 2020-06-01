7cbf0b3b71b45cf185489a54858b1c9e9fceb41d
Boiko33 <theboy4ik33@gmail.com>
2020-06-01 11:27:52 +0300

    Was added ScrollToTop function

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import AboutMe from './components/NavBar/AboutMe/AboutMe';
 import Blog from './components/NavBar/Blog/Blog';
 import OnlineBlock from './components/NavBar/OnlineBlock/OnlineBlock';
 
 const App = (props) => {
-  
-  
+
+
   return (
-    <BrowserRouter>
     <div className="body">
       <NavBar />
       <Route component={AboutMe} path="/aboutme"></Route>
       <Route component={Body} path="/main"></Route>
-      <Route path="/blog" render={() => <Blog blogData={props.blogData}/>} />
+      <Route path="/blog" render={() => <Blog blogData={props.blogData} />} />
       <Route component={OnlineBlock} path="/online"></Route>
       <Footer />
     </div>
-    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
- import blogData from "./state"
- 
+import store from "./store";
+import { BrowserRouter } from 'react-router-dom';
+import { ScrollToTop } from "./store.js";
 
 
 ReactDOM.render(
-<App blogData={blogData} />
-, document.getElementById('root'));
+    <BrowserRouter>
+        <ScrollToTop />
+        <App blogData={store.state.blogData} />
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 
 serviceWorker.unregister();
